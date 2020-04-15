aaa321ca694c458eb5ac6bb8a8076246e9484156
Vladyslav <vladyslav.work@gmail.com>
2020-04-15 23:55:38 +0200

    added basic navigation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,23 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => (
   <nav className={styles.navbar}>
-    <div className={styles.navbarLogo}>Note App</div>
+    <div className={styles.navbarLogo}>
+      <img src={require("../../images/logo.png")} alt="Note app logo" />
+    </div>
+    <ul className={styles.navbarList}>
+      <li className={styles.navItem}>
+        <NavLink className={styles.navLink} to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li className={styles.navItem}>
+        <NavLink className={styles.navLink} to="/about" exact>
+          About
+        </NavLink>
+      </li>
+    </ul>
   </nav>
 );
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,30 @@
 import React from "react";
+import styles from "./About.module.css";
 
 export const About = () => {
   return (
     <>
-      <h2>About page</h2>
+      <div className={styles.wrapper}>
+        <img
+          src={require("../../images/logo.png")}
+          alt="Note app logo"
+          className={styles.logo}
+        />
+        <h2 className={styles.title}>Note App</h2>
+        <p className={styles.version}> Version 1.2 (201.3)</p>
+        <p className={styles.autor}>
+          Copyright &copy; 2020{" "}
+          <a
+            href="https://github.com/VladyslavGit"
+            target="blanc"
+            rel="noopener noreferrer"
+            className={styles.link}
+          >
+            Vladyslav inc.
+          </a>
+          <span className={styles.span}>All rights reserved.</span>
+        </p>
+      </div>
     </>
   );
 };
